17b0c48c6abb6f4467ecf7996f074a78fb1eaf86
Bob McClellan Jr <bob.mcclellan@gmail.com>
2013-12-04 23:28:13 -0600

    Listing of vault names in glaciergit status

---
+++ aws-glacier.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "bundler/setup"
+require "aws-sdk"
+require "yaml"
+
+# load credentials
+creds = YAML::load_file "aws_credentials.yml"
+
+# instantiate glacier object
+glacier = AWS::Glacier.new(access_key_id: creds['access_key'], secret_access_key: creds['secret_key'])
+
+# loop through vaults
+glacier.vaults.each do |vault|
+  puts vault.name
+end
